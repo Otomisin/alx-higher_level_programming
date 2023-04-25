628790ac95ec1ecc6140e5e51fd71a7061e54192
otomisin <orenaiket@yahoo.com>
2023-04-25 19:02:07 +0100

    Task 5-to_integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const num = Math.floor(Number(process.argv[2]));
-console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
+const num = Math.floor(Number(process.argv[2])); //  Number() to convert the first argument to a number. If the argument cannot be converted to a number, Number() returns NaN. it also rounds down the number to an integer.
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`); // The $ sign and curly braces (${}) are used to create a template literal in JavaScrip.  isNaN(num) checks if num is true
