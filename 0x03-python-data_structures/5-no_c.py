567dd95fc2b99e076ba4502726efafb414a86277
otomisin <orenaiket@yahoo.com>
2023-04-30 16:19:28 +0100

    Task 5-no_c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,8 @@
 """
 Write a function that removes all characters c and C from a string.
 """
+
+
 def no_c(my_string):
     new_s = my_string.translate({ord('c'): None})
     new_s = new_s.translate({ord('C'): None})
